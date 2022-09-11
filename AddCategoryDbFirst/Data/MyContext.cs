6d4d5df9590d6280cbbc748ae0abb3037e531ba9
khanimgurbanli <zeri.q.222@gmail.com>
2022-09-11 13:34:40 +0400

    first commit

---
+++ AddCategoryDbFirst/Data/MyContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddCategoryDbFirst.Data
+{
+    public class MyContext: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+                optionsBuilder.UseSqlServer(@"Server=.\SQLExpress;Database=Northwind;Trusted_Connection=True;");
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ AddCategoryDbFirst/Program.cs
@@ -0,0 +1,34 @@
+﻿using AddCategoryDbFirst.Models;
+using System.Drawing;
+
+namespace AddCategoryDbFirst
+{
+    class Program
+    {
+        public static void Main()
+        {
+            NorthwindContext db = new();
+            var categories = db.Categories.ToList();
+
+            foreach (var catgr in categories)
+             db.Categories.Add(new Category
+             {
+                 CategoryName = "Task category",
+                 Description = "For task description"
+             });
+
+
+            bool returnResult = db.SaveChanges() > 0;
+
+                Console.BackgroundColor =ConsoleColor.Green;
+            if (returnResult)
+                    Console.WriteLine("Succesfully added category ");
+            else
+                Console.WriteLine("Warning, chack prosses and try again");
+            Console.ResetColor();
+            var a = db.Categories.FirstOrDefault(x => x.CategoryName == "Task category");
+            Console.WriteLine($"Catgory Name: {a.CategoryName} \nCategory Description: {a.Description}");
+        }
+    }
+}
+
